bd6a0a6e0be8229b98a31c8f7397a856b046d9f5
JiwonHae94 <hjiwon1994@gmail.com>
2022-06-27 16:49:05 +0900

    feat(api): implement github user request

--- lib/bloc/github_user_bloc.dart
+++ lib/bloc/github_user_bloc.dart
@@ -7,14 +7,17 @@ import '../data/repositories/common/github_sort.dart';
 
 part "github_user_state.dart";
 
-class GithubUserCubit extends Cubit<List<int>>{
+class GithubUserCubit extends Cubit<List<GithubUser>>{
   GithubUserCubit(this.githubUserUsecase) : super(List.empty());
 
   final GithubUserUsecase githubUserUsecase;
 
   Future<void> search(String keyword,{ GithubOrder order = GithubOrder.DESC, GithubSort? sort = null, int perPage = 30, int page = 1}) async{
     final users = await githubUserUsecase.call(keyword: keyword, page: page, perPage: perPage, sort: sort, order : order);
-    emit([1,2,3,4,5,6,7,1,10]);
+
+    emit(
+      users
+    );
   }
 
 }
